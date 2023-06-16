b3c2f29a58cccf90a502507422a29525ba9942f5
Zhalhou-dev <80689458+Zhalhou-dev@users.noreply.github.com>
2023-06-16 17:41:15 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -12,41 +12,51 @@ def index():
     return render_template('home.html')
 
 
-# Define route and Verify_otp() function below
-
+# Define Verify_otp() function
+@app.route('/login' , methods=['POST'])
 def verify_otp():
-	username-request.form['username']
-	password=request.form['password']
-	mobile_number=request.form['number']
-	if username=="Zhalhou" and password=="12345":
-		Twilio_SID="AC2a4e0722722d55ace198db1858312dd9"
-		Twilio_auth_token="a40c3debc16b9ae03e730e02a87b0065"
-		client=Client(Twilio_SID,Twilio_auth_token)
-		        verification = client.verify \
-            .services(Twilio_SID) \
+    username = request.form['username']
+    password = request.form['password']
+    mobile_number = request.form['number']
+
+    if username == 'verify' and password == '12345':   
+        account_sid = 'AC2a4e0722722d55ace198db1858312dd9'
+        auth_token = 'a40c3debc16b9ae03e730e02a87b0065'
+        client = Client(account_sid, auth_token)
+
+        verification = client.verify \
+            .services('VA0b1d7ce0f1ab38138bdd2de65ec5427a') \
             .verifications \
             .create(to=mobile_number, channel='sms')
 
-
-		return render_template('home.html')
-	else:
-		"Entered password or username is wrong"
-
-
+        print(verification.status)
+        return render_template('otp_verify.html')
+    else:
+        return render_template('user_error.html')
 
 
 
+@app.route('/otp', methods=['POST'])
+def get_otp():
+    print('processing')
 
+    received_otp = request.form['received_otp']
+    mobile_number = request.form['number']
+    account_sid = 'AC2a4e0722722d55ace198db1858312dd9'
+    auth_token = 'a40c3debc16b9ae03e730e02a87b0065'
 
+    client = Client(account_sid, auth_token)
+                                            
+    verification_check = client.verify \
+        .services('VA0b1d7ce0f1ab38138bdd2de65ec5427a') \
+        .verification_checks \
+        .create(to=mobile_number, code=received_otp)
+    print(verification_check.status)
 
-
-
-
-
-
-
-
-
+    if verification_check.status == "pending":
+        return render_template('otp_verify.html','otp_error.html')    # Write code here
+    else:
+        return redirect("https://collaborative-document-y68a.onrender.com/")
 
 
 if __name__ == "__main__":
